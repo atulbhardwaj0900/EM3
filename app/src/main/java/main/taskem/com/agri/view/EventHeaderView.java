bc7b605b84e5864e885d1606664948b0fcf9e613
Atul Bhardwaj <bhardwaj.atul09@gmail.com>
2016-05-29 01:19:12 +0530

    Added Header custom view components

---
+++ app/src/main/java/main/taskem/com/agri/view/EventHeaderView.java
@@ -0,0 +1,27 @@
+package main.taskem.com.agri.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+/**
+ * Created by atul.bhardwaj on 28/05/16.
+ */
+public class EventHeaderView  extends RelativeLayout {
+
+    public EventHeaderView(Context context) {
+        super(context);
+    }
+
+    public EventHeaderView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public EventHeaderView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public EventHeaderView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
