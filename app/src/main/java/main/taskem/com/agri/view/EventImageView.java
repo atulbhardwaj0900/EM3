af8f281ff7090ae090b7abe6fabb6148a7d64944
Atul Bhardwaj <atul.bhardwaj@snapdeal.com>
2016-05-30 16:16:32 +0530

    no message

--- app/src/main/java/main/taskem/com/agri/view/EventImageView.java
+++ app/src/main/java/main/taskem/com/agri/view/EventImageView.java
@@ -2,15 +2,17 @@ package main.taskem.com.agri.view;
 
 
 import android.content.Context;
+import android.net.Uri;
 import android.util.AttributeSet;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Picasso.Listener;
 
 /**
  * Created by atul.bhardwaj on 30/05/16.
  */
-public class EventImageView extends ImageView {
+public class EventImageView extends ImageView implements Listener {
 	private Context context;
 
 	public EventImageView(Context context) {
@@ -29,6 +31,12 @@ public class EventImageView extends ImageView {
 	}
 
 	public void setImage(String url) {
+
 		Picasso.with(context).load(url).into(this);
+	}
+
+	@Override
+	public void onImageLoadFailed(Picasso picasso, Uri uri, Exception exception) {
+
 	}
 }
